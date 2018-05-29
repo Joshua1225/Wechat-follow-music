c2f5850bd7cc767256b49438e7ec6134ac9e165f
Joshua <xiaodaiyue@foxmail.com>
2018-05-29 16:46:01 +0800

    Signed-off-by: Joshua <xiaodaiyue@foxmail.com>

--- client/app.js
+++ client/app.js
@@ -3,61 +3,38 @@ var qcloud = require('./vendor/wafer2-client-sdk/index')
 var config = require('./config')
 
 App({
+  onLaunch: function () {
+    var islogin=false;
+    try {
+		  //提取缓存
+      var value = wx.getStorageSync('userid')
+      console.log(value)
 
-
-
-    onLaunch: function () {
-      //检查登录密钥userid是否存在且有效
-      var islogin=false;
-      try {
-		  //检查缓存
-        var value = wx.getStorageSync('userid')
-        console.log(value)
-
-        if (value=='') {
-          
-            wx.request({
-              url: config.service.isloginUrl,
-              data:{
-                userid:value
-              },
-              success:function(res)
-              {
-                console.log(res)
-                if(res.data=='bool(true)')
-                {
-                  islogin=true
-                  console.log(islogin)
-                }
-                //未登录需要登录
-                if (islogin == false) {
-                  //console.log(islogin)
-                  wx.login({
-                    success: function (res) {
-                      console.log(res)
-                      wx.request({
-                        url: config.service.loginUrl,
-                        data: {
-                          code: res.code
-                        },
-                        success: function (response) {
-                          try {
-                            wx.setStorageSync('userid', response.data)
-                          } catch (e) {
-                            console.log('error')
-                          }
-                        }
-
-                      })
-                    }
-                  })
-                }  
+      wx.request({//检查userid是否有效
+        url: config.service.isloginUrl,
+        data:{
+          userid:value
+        },
+        success: function (res1) {//回调函数获取结果处理
+          //console.log(res1)
+          if(res1.data=='(bool)false'){//失效-重新获取code登录
+            wx.login({
+              success:function(res2){//回填函数向服务器转发code换取3rd-key
+                wx.request({
+                  url: config.service.loginUrl,
+                  data:{
+                    code:res2.code
+                  },
+                  success: function (res3) {//回填函数userid存入缓存
+                    wx.setStorageSync('userid', res3.data)
+                    console.log(res3.data)
+                  }
+                })
               }
             })
           }
-
-       
-      
+        }
+      })
     } catch (e) {
       console.log('userid not exist')
     }
