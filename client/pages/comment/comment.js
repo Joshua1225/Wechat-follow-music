e7ef9e6a5fa6439bfee3e9b4be8380271ca5b8e7
Joshua <xiaodaiyue@foxmail.com>
2018-06-15 23:58:46 +0800

    Signed-off-by: Joshua <xiaodaiyue@foxmail.com>

--- client/pages/comment/comment.js
+++ client/pages/comment/comment.js
@@ -65,7 +65,7 @@ Page({
       },
       success: function (res) {
         console.log("comment err");
-        console.log(res.data[0].UserInfo['nickName']);
+        console.log(res.data);
         that.setData({
           musicName: res.data[0].MusicName,
           musicSinger: res.data[0].MusicSinger,
